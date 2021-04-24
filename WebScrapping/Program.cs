6640a7bc6347b7ae8afd5c60de27251ef14fd182
Florencia Hollmann <florh89@gmail.com>
2021-04-23 21:39:25 -0300

    Add project files.

---
+++ WebScrapping/Program.cs
@@ -0,0 +1,18 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using System;
+using System.Compat.Web;
+using System.Net;
+
+namespace WebScrapping
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+            Console.Read();
+        }
+    }
+}
